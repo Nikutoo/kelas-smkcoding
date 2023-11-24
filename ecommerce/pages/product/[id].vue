abdae4861029026f3c7bdd31a739fb027d7ad4bb
Nikutoo <rakaskymo@gmail.com>
2023-11-24 14:12:25 +0700

    Update

--- ecommerce/pages/product/[id].vue
+++ ecommerce/pages/product/[id].vue
@@ -3,9 +3,9 @@ import { products } from "~/composables/constants/products";
 import { Products } from "~/types/products";
 const route = useRoute();
 const id = typeof route.params.id === 'string' ? parseInt(route.params.id) :
-null;
+    null;
 const product = ref(products.find((item: Products) => item.id === id));
 </script>
 <template>
-<DetailProduct :product="product"/>
+    <DetailProduct :product="product" />
 </template>
